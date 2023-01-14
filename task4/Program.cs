8324debce2115a33a71b5c4eaa1299798f9aaecf
Alex Rokachev <alserokachev@gmail.com>
2023-01-14 13:52:54 +0300

    fourth task

---
+++ task4/Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (number > 1) {
+    if (number % 2 == 0)
+    {
+        while (number > 1)
+        {
+            System.Console.WriteLine(number);
+            number -= 2;
+        }
+    }
+    else
+    {
+        number -= 1;
+        while (number > 1)
+        {
+            System.Console.WriteLine(number);
+            number -= 2;
+        }
+    }
+}
+else
+{
+System.Console.WriteLine("Нужно вводить числа больше 1");
+}
